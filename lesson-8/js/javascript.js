985e7ddd7e70420e35473574258cae20768f4f3f
KnightenCooper <coo18028@byui.edu>
2021-02-27 13:05:44 -0600

    button troubleshooting

--- lesson-8/js/javascript.js
+++ lesson-8/js/javascript.js
@@ -58,7 +58,3 @@ document.getElementById("windChill").innerText = windChill;
 function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-document.getElementById("feedback").onclick = function () {
-  location.href = "www.youtube.com";
-}
