037ad3850d6a3d830e24074059c99931f5f86267
KnightenCooper <coo18028@byui.edu>
2021-03-05 01:08:41 -0600

    remove bad link, fix accessibilty issue

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -11,7 +11,7 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
-      let h4 = document.createElement("h4");
+      let h4 = document.createElement("h3");
       let h5 = document.createElement("h4");
       let img = document.createElement("img");
 
