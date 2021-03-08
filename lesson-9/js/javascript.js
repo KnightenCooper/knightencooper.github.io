bb00a2fbd8a0d33d8293e809e822baecfcc2de71
KnightenCooper <coo18028@byui.edu>
2021-03-08 00:22:00 -0600

    updated homepage

--- lesson-9/js/javascript.js
+++ lesson-9/js/javascript.js
@@ -39,29 +39,30 @@ fetch(requestURL)
     .then(function(jsonObject) {
         console.table(jsonObject);
         const towns = jsonObject['towns'];
-        const townsA = [5, 1, 6]
-        for (let i = 0; i < townsA.length; i++) {
-            let information = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let h3 = document.createElement('h3');
-            let h3_1 = document.createElement('h4');
-            let h3_2 = document.createElement('h4');
-            let h3_3 = document.createElement('h4');
+        var town_numbers = [6, 5, 1]
+        for (let i = 0; i < town_numbers.length; i++) {
+            let information = document.createElement('article');
+            let name = document.createElement('h2');
+            let motto = document.createElement('h3');
+            let year = document.createElement('h4');
+            let population = document.createElement('h4');
+            let rainfall = document.createElement('h4');
             let img = document.createElement('img');
 
-            h2.textContent = towns[townsA[i]].name;
-            h3.textContent = '"' + towns[townsA[i]].motto + '"';
-            h3_1.textContent = 'Year Founded: ' + towns[townsA[i]].yearFounded;
-            h3_2.textContent = 'Population: ' + towns[townsA[i]].currentPopulation;
-            h3_3.textContent = 'Annual Rainfall: ' + towns[townsA[i]].averageRainfall + '"';
-            img.setAttribute('src', 'images/' + towns[townsA[i]].photo);
-            img.setAttribute('alt', towns[townsA[i]].name + 'photo');
 
-            information.appendChild(h2);
-            information.appendChild(h3);
-            information.appendChild(h3_1);
-            information.appendChild(h3_2);
-            information.appendChild(h3_3);
+            name.textContent = towns[town_numbers[i]].name;
+            motto.textContent = '"' + towns[town_numbers[i]].motto + '"';
+            year.textContent = 'Year Founded: ' + towns[town_numbers[i]].yearFounded;
+            population.textContent = 'Population: ' + towns[town_numbers[i]].currentPopulation;
+            rainfall.textContent = 'Annual Rainfall: ' + towns[town_numbers[i]].averageRainfall + '"';
+            img.setAttribute('src', 'images/' + towns[town_numbers[i]].photo);
+            img.setAttribute('alt', towns[town_numbers[i]].name + 'photo');
+
+            information.appendChild(name);
+            information.appendChild(motto);
+            information.appendChild(year);
+            information.appendChild(population);
+            information.appendChild(rainfall);
             information.appendChild(img);
 
             document.querySelector('div.towns').appendChild(information);
