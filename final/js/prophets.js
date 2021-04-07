400f9f29cf457c385c23e45237422dedcfa497f6
KnightenCooper <coo18028@byui.edu>
2021-04-06 20:29:53 -0500

    update name

--- final/js/prophets.js
+++ final/js/prophets.js
@@ -6,26 +6,26 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const prophets = jsonObject["prophets"];
+    const company = jsonObject["company"];
     console.table(jsonObject);
-    for (let i = 0; i < prophets.length; i++) {
+    for (let i = 0; i < company.length; i++) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let h3 = document.createElement("h3");
       let h4 = document.createElement("h4");
       let img = document.createElement("img");
 
-      h2.textContent = prophets[i].name + " " + prophets[i].lastname;
-      h3.textContent = "Day of Birth: " + prophets[i].birthdate;
-      h4.textContent = "Location of Birth: " + prophets[i].birthplace;
-      img.setAttribute("src", prophets[i].imageurl);
+      h2.textContent = company[i].name + " " + company[i].lastname;
+      h3.textContent = "Day of Birth: " + company[i].birthdate;
+      h4.textContent = "Location of Birth: " + company[i].birthplace;
+      img.setAttribute("src", company[i].imageurl);
       img.setAttribute(
         "alt",
-        prophets[i].name +
+        company[i].name +
           " " +
-          prophets[i].lastname +
+          company[i].lastname +
           " - " +
-          prophets[i].order
+          company[i].order
       );
 
       card.appendChild(h2);
